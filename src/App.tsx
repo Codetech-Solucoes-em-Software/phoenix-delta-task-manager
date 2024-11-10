47f7c68e65a7ee8c99c64f72530de3eb50bc4dee
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-10 17:53:51 -0300

    feat: add AuthContext and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import { UserRole } from "./enums/UserRole";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Admin from "./pages/admin/Admin";
+import Home from "./pages/home/Home";
+
+const PrivateRoute = ({ children, role }: { children: JSX.Element, role: UserRole }) => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" />;
+  if (user.role !== role) return <Navigate to="/" />;
+  return children;
+};
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<PrivateRoute role={UserRole.admin}><Admin /></PrivateRoute>} />
+        <Route path="/" element={<PrivateRoute role={UserRole.user}><Home /></PrivateRoute>} />
+      </Routes>
+    </Router>
   );
 }
 
