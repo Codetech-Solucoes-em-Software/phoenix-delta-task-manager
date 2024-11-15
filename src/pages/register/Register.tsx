06a34353a8433ad4cabf2043fe1e744cad9b8720
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-15 14:34:03 -0300

    feat: register page

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,5 +1,106 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useNavigate } from "react-router-dom";
+import { User, useAuth } from "../../context/AuthContext";
+import { useState } from "react";
+import { userExists, createUser, getUser } from "../../services/UserService";
+import { generateRefreshToken, generateToken } from "../../services/AuthService";
+
 export default function Register() {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+  
+  const [name, setName] = useState('');
+  const [degree, setDegree] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [role, setRole] = useState<'admin' | 'user'>('user');
+  const [lodge, setLodge] = useState<'phoenix' | 'delta'>('phoenix');
+  const [error, setError] = useState('');
+
+  const handleRegister = () => {
+    if (!email || !password) {
+      setError('Por favor, preencha todos os campos.');
+      return;
+    }
+
+    if (userExists(email)) {
+      setError('Usuário já existe.');
+      return;
+    }
+
+    const newUser = {
+      id: new Date().getDate().toString(),
+      name,
+      degree,
+      lodge,
+      email,
+      password,
+      role,
+      token: generateToken(email),
+      refreshToken: generateRefreshToken(email),
+    };
+    
+    if (!createUser(newUser)) {
+      setError('Erro ao criar usuário.');
+      return;
+    }
+
+    const registeredUser = getUser(email);
+
+    if (registeredUser) {
+      login({ email, role, token: registeredUser.token });
+      navigate(role === 'admin' ? '/admin' : '/home');
+    }
+  };
   return (
-    <></>
+    <div style={{ display: 'flex', width: 400, height: 400, flexDirection: 'column', padding: 30, alignItems: 'center', justifyContent: 'center', gap: 6, border: 'none',
+    borderRadius: 14, backgroundColor: '#232323' }}>
+      <h2>Registrar</h2>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <label htmlFor="name">
+        Nome:
+        <input
+          type="text"
+          placeholder="Type your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label htmlFor="degree">
+        Grau:
+        <input
+          type="text"
+          placeholder="Type your degree"
+          value={degree}
+          onChange={(e) => setDegree(e.target.value)}
+        />
+      </label>
+      <label htmlFor="email">
+        E-mail:
+        <input
+          type="text"
+          placeholder="Type your e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          placeholder="Type your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label htmlFor="lodge">
+        Loja:
+        <select onChange={(e) => setLodge(e.target.value as 'phoenix' | 'delta')}>
+          <option value="phoenix">Phoenix</option>
+          <option value="delta">Delta</option>
+        </select>
+      </label>
+      <button onClick={handleRegister}>Registrar</button>
+    </div>
   );
 }
