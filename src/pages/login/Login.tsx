aa6c8b4edcc961bac8d8f3337a8ef09100ad455b
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-15 14:27:44 -0300

    feat: validations

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+// import { getUser } from "../../services/UserService";
+import { authenticateUser } from "../../services/AuthService";
 
 export default function Login() {
+  const navigate = useNavigate();
   const { login } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleLogin = () => {
-    email === 'admin@admin.com' && password === '123456' ? 
-    login({ email, role: 'admin', password}) : 
-    login({ email, role: 'user', password}) ;
+    const user = authenticateUser(email, password);
+
+    if (!user) {
+      setError('Usuário ou senha incorretos.');
+    }
+
+    login({ email: user?.email, role: user?.role, token: user?.token });
+
+    navigate(user?.role === 'admin' ? '/admin' : '/home');
   };
   return (
     <>
+      <h1>Login</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <div>
         <input type="email" name="email" placeholder="Type your e-mail" onChange={(e) => setEmail(e.target.value)}/>
       </div>
