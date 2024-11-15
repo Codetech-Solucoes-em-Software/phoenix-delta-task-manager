2a8a7b105a9b3e9cc56fb77ca75c8c24fc3f1b10
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-15 11:07:59 -0300

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import Admin from "./pages/admin/Admin";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
 function App() {
   return (
     <div className="App">
+      {/* <Admin /> */}
+      <Login />
+      {/* <Register /> */}
+      {/* <Home /> */}
     </div>
   );
 }
