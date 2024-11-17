e38235949c07189fe2ce60bf4658a5e658cee33c
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-16 22:42:34 -0300

    feat: logo and more details of page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 // import { getUser } from "../../services/UserService";
 import { authenticateUser } from "../../services/AuthService";
 import useDocumentTitle from "../../hooks/PageTitle";
+import MainLogo from '../../assets/331d4371a7b3d149e94095a89c372632.jpg';
+import { styles } from "./styles";
 
 export default function Login() {
   useDocumentTitle('Login');
@@ -13,28 +15,44 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    const user = authenticateUser(email, password);
-
-    if (!user) {
-      setError('Usuário ou senha incorretos.');
+  const handleLogin = async () => {
+    try {
+      const user = await authenticateUser(email, password);
+  
+      if (!user) {
+        setError('Usuário ou senha incorretos.');
+        return;
+      }
+  
+      login({ email: user.email, role: user.role, token: user.token });  
+      navigate(user.role === 'admin' ? '/admin' : '/home');
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      setError('Ocorreu um erro ao tentar fazer login.');
     }
-
-    login({ email: user?.email, role: user?.role, token: user?.token });
-
-    navigate(user?.role === 'admin' ? '/admin' : '/home');
   };
   return (
-    <>
-      <h1>Login</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <div>
-        <input type="email" name="email" placeholder="Type your e-mail" onChange={(e) => setEmail(e.target.value)}/>
+    <div style={styles.container}>
+      <div className={`
+        col-sm-5 d-flex justify-content-center
+      `}>
+        <form action="" style={styles.loginForm}>
+          <h1>Login</h1>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <div>
+            <input type="email" name="email" placeholder="Type your e-mail" onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div>
+            <input type="password" name="" placeholder="Type your password" onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <button onClick={handleLogin}>Login</button>
+        </form>
       </div>
-      <div>
-        <input type="password" name="" placeholder="Type your password" onChange={(e) => setPassword(e.target.value)}/>
+      <div className={`
+        col-sm-7 d-flex
+      `}>
+        <img src={MainLogo} alt="Logo Principal" style={styles.logo} />
       </div>
-      <button onClick={handleLogin}>Login</button>
-    </>
+    </div>
   );
 }
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,5 +1,39 @@
 export const styles = {
   container: {
+    display: 'flex' as 'flex'
+  },
+  imageContainer: {
     
+  },
+  logo: {
+    width: '100%',
+    height: '100vh'
+  },
+  loginForm: {
+    display: 'flex' as 'flex',
+    flexDirection: 'column' as 'column',
+    justifyContent: 'center' as 'center',
+    alignItems: 'center' as 'center',
+    gap: 8
+  },
+  input: {
+    width: 350,
+    height: 30,
+    paddingLeft: 7,
+    borderRadius: 10,
+    border: 'none' as 'none',
+    outline: 'none' as 'none',
+    backgroundColor: '#535353' as string,
+    color: '#f5f5f5' as string
+  },
+  button: {
+    width: 350,
+    height: 30,
+    backgroundColor: '#252525' as string,
+    border: 'none' as 'none',
+    borderRadius: 10,
+    ':hover': {
+      backgroundColor: '#121212' as string
+    }
   }
 };
