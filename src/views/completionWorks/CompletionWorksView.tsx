2228fc858cb12640867fdd1d5a756fd0f6f55e0e
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-01-22 19:57:50 -0300

    feat: add views of pages

---
+++ src/views/completionWorks/CompletionWorksView.tsx
@@ -0,0 +1,5 @@
+import CompletionWorks from "../../pages/tasks/completionWorks/CompletionWorks";
+
+export default function CompletionWorksView() {
+  return <CompletionWorks />
+}
---
+++ src/views/visitations/VisitationsView.tsx
@@ -0,0 +1,5 @@
+import Visitations from "../../pages/tasks/visitations/Visitations";
+
+export default function VisitationsView() {
+  return <Visitations />
+}
