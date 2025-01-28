52795d512f1b4193575a56f1cd7ae044648f2cec
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-01-28 20:51:57 -0300

    feat: add api configurations for frontend

---
+++ src/services/Api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API_URL_BASE = 'http://localhost:4000'; // Substitua pela URL da sua API
+
+const api = axios.create({
+  baseURL: API_URL_BASE,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default api;
