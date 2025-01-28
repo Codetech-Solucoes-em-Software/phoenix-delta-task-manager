3e21c5d6e9bf3a3fe74365f78269ebc9adfd1358
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-01-28 20:52:26 -0300

    refac: authenticateUser and interface

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,10 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { User } from '../models/User';
+import api from './Api';
 import { getAllUsers } from './UserService';
 import { getUserByEmail, updateUser } from './UserService';
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const TOKEN_EXPIRATION_TIME = 60 * 60 * 1000;
+
+interface AuthResponse {
+  access_token: { access_token: string; user: any }; 
+}
+
 
 const generateToken = (email: string): string => {
   return `${email}-${Math.random().toString(36).substr(2, 9)}`;
@@ -14,19 +18,17 @@ const generateRefreshToken = (email: string): string => {
   return `${email}-refresh-${Math.random().toString(36).substr(2, 9)}`;
 };
 
-const authenticateUser = async (email: string, password: string): Promise<User | null> => {
-  if (!email) return null;
-  const user = await getUserByEmail(email);
-
-  if (!user || user.password !== password) return null;
-
-  const token = generateToken(email);
-  const refreshToken = generateRefreshToken(email);
-
-  const updatedUser = { ...user, token, refreshToken };
-  await updateUser(updatedUser);
-
-  return updatedUser;
+const authenticateUser = async (data: any): Promise<AuthResponse> => {
+  try {
+    const response: any = await api.post(`/login`, data);
+    if (!response && !response.data) {
+      console.error('Resposta inválida da API:', response);
+    }
+    return response.data;
+  } catch (error: any) {
+    console.error('Erro durante a autenticação:', error.message);
+    return error;
+  }
 };
 
 const validateToken = async (token: string): Promise<boolean> => {
