747f34206cc72519f25e88f3a471fd6ee9af36f9
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-02-01 16:14:51 -0300

    feat: add updateInstruction route and add comments for divide types of routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import CreateInstruction from "./pages/tasks/TaskPage";
 import CreateCompletionWork from "./pages/tasks/completionWorks/createCompletionWork/CreateCompletionWork";
 import CreateReading from "./pages/tasks/readings/createReading/CreateReading";
 import Register from "./pages/register/Register";
+import UpdateInstruction from "./pages/tasks/instructions/updateInstruction/UpdateInstruction";
 
 const PrivateRoute = ({ children, role }: { children: JSX.Element; role: 'ADMIN' | 'USER' }) => {
   const { user }: any = useAuth();
@@ -42,8 +43,12 @@ function App() {
         }
       />
       <Route path="/admin" element={<PrivateRoute role="ADMIN"><Admin /></PrivateRoute>} />
+      {/* Rotas Instructions */}
       <Route path="/admin/createInstruction" element={<PrivateRoute role="ADMIN"><CreateInstruction /></PrivateRoute>} />
+      <Route path="/admin/updateInstruction/:id" element={<PrivateRoute role="ADMIN"><UpdateInstruction /></PrivateRoute>} />
+      {/* Rotas CompletionWork */}
       <Route path="/admin/createCompletionWork" element={<PrivateRoute role="ADMIN"><CreateCompletionWork /></PrivateRoute>} />
+      {/* Rotas Readings */}
       <Route path="/admin/createReading" element={<PrivateRoute role="ADMIN"><CreateReading /></PrivateRoute>} />
       <Route path="/home" element={<PrivateRoute role="USER"><Home /></PrivateRoute>} />
       <Route path="/instructions" element={<InstructionView />} />
