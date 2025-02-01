056dd2ee6c8928856567e267632aa8fc96984684
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-02-01 11:21:47 -0300

    fix: set user.role for loading instructions, treatment of api data

--- src/pages/tasks/instructions/Instructions.tsx
+++ src/pages/tasks/instructions/Instructions.tsx
@@ -13,17 +13,15 @@ interface Instruction {
   name: string;
   expected_date: string;
   finished_date: string;
-  approved_date: string;
   status: string;
 }
 
 export default function Instructions() {
-  useDocumentTitle('Instruções Clássicas');
+  useDocumentTitle("Instruções Clássicas");
   const { user } = useAuth();
+  const navigate = useNavigate();
   const [instructions, setInstructions] = useState<Instruction[]>([]);
   const [loading, setLoading] = useState(true);
-  const [file, setFile] = useState(null);
-  const navigate = useNavigate();
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -31,71 +29,48 @@ export default function Instructions() {
       getInstructions(user.role, user.id)
         .then((data: any) => {
           console.log("📌 Instruções carregadas:", data);
-          setInstructions(data.classicalLessons);
+          if (user.role === 'ADMIN') {
+            setInstructions(Array.isArray(data.classicalLessons) ? data.classicalLessons : []);
+          } else {
+            setInstructions(data.lessonsUser ? [data.lessonsUser] : []);
+          }
         })
-        .catch((error: any) => console.error("🚨 Erro ao buscar instruções:", error))
+        .catch((error) => console.error("🚨 Erro ao buscar instruções:", error))
         .finally(() => setLoading(false));
     }
   }, [user]);
 
   if (!user) {
-    return <p style={{ textAlign: "center", marginTop: 20 }}>Carregando usuário...</p>;
+    return (
+      <p style={{ textAlign: "center", marginTop: 20 }}>Carregando usuário...</p>
+    );
   }
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fileData: any = e.target.files?.[0];
-    if (fileData) {
-      setFile(fileData);
-      handleUpload(fileData);
-    }
-  };
-
-  const handleButtonClick = () => {
-    fileInputRef.current?.click();
-  }
-
-  const handleUpload = async (file: File) => {
-    if (file) {
-      try {
-        const result = await uploadFile(file, user.id); // Chama a função da service
-        console.log('Arquivo enviado com sucesso:', result);
-      } catch (error) {
-        console.error('Erro ao enviar o arquivo:', error);
-      }
-    } else {
-      console.error('Nenhum arquivo selecionado');
-    }
-  };
-
-
-  const getStatusBadgeClasses = (status: string) => {
-    switch (status) {
-      case 'CONCLUIDO':
-        return { ...styles.statusBadge, ...styles.statusBadgeConcluido };
-      case 'REVISAR':
-        return { ...styles.statusBadge, ...styles.statusBadgeRevisar }
-      case 'AGUARDANDO':
-        return { ...styles.statusBadge, ...styles.statusBadgeAguardando }
-      case 'PENDENTE':
-        return { ...styles.statusBadge, ...styles.statusBadgeAguardando }
-      default:
-        return styles.statusBadge;
-    }
-  };
 
   return (
     <div style={styles.container}>
       {/* Cabeçalho */}
       <div style={styles.header}>
-        <h1 style={styles.headerTitle}>A sua Jornada do Conhecimento começa aqui:</h1>
+        <h1 style={styles.headerTitle}>
+          A sua Jornada do Conhecimento começa aqui:
+        </h1>
         <div style={styles.headerInfo}>
           <div style={styles.inputGroup}>
             <label style={styles.inputGroupLabel}>Nome:</label>
-            <input type="text" value={user?.name ?? ""} readOnly style={styles.inputGroupInput} />
+            <input
+              type="text"
+              value={user?.name ?? ""}
+              readOnly
+              style={styles.inputGroupInput}
+            />
           </div>
           <div style={styles.inputGroup}>
             <label style={styles.inputGroupLabel}>Grau:</label>
-            <input type="text" value={user?.degree ?? ""} readOnly style={styles.inputGroupInput} />
+            <input
+              type="text"
+              value={user?.degree ?? ""}
+              readOnly
+              style={styles.inputGroupInput}
+            />
           </div>
           <img src={logoLojas} style={styles.headerImage} />
         </div>
@@ -103,7 +78,15 @@ export default function Instructions() {
 
       {/* Mensagem de carregamento */}
       {loading ? (
-        <p style={{ textAlign: "center", marginTop: 20 }}>Carregando instruções...</p>
+        <p style={{ textAlign: "center", marginTop: 20 }}>
+          Carregando instruções...
+        </p>
+      ) : instructions.length === 0 ? (
+        <p style={{ textAlign: "center", marginTop: 20 }}>
+          {user.role === "ADMIN"
+            ? "Nenhuma instrução encontrada."
+            : "Não há instruções registradas para o usuário."}
+        </p>
       ) : (
         <>
           {/* Tabela de Instruções */}
@@ -114,58 +97,61 @@ export default function Instructions() {
               <div style={styles.dateCol}>Data de Entrega</div>
               <div style={styles.statusCol}>Situação</div>
             </div>
-            {instructions.length > 0 ? (
-              instructions.map((item) => (
-                <div key={item.id} style={styles.instructionRow}>
-                  <div style={styles.instructionsCol}>
-                    <p style={styles.instructionsColP}>{item.name}</p>
-                  </div>
-                  <div style={styles.dateCol}>
-                    <input
-                      type="text"
-                      value={item.expected_date ? new Date(item.expected_date).toLocaleDateString() : ""}
-                      readOnly
-                      style={styles.dateColInput}
-                    />
-                  </div>
-                  <div style={styles.dateCol}>
-                    <input
-                      type="text"
-                      value={item.finished_date ? new Date(item.finished_date).toLocaleDateString() : ""}
-                      readOnly
-                      style={styles.dateColInput}
-                    />
-                  </div>
-                  <div style={styles.uploadCol}>
-                    <button style={styles.uploadColButton} onClick={handleButtonClick}>Upload</button>
-                    <input
-                      type="file"
-                      ref={fileInputRef}
-                      style={{ display: 'none' }}
-                      onChange={handleFileChange}
-                    />
-                  </div>
-                  <div style={styles.statusCol}>
-                    <span style={getStatusBadgeClasses(item.status)}>{item.status}</span>
-                  </div>
+            {instructions.map((item) => (
+              <div key={item.id} style={styles.instructionRow}>
+                <div style={styles.instructionsCol}>
+                  <p style={styles.instructionsColP}>{item.name}</p>
                 </div>
-              ))
-            ) : (
-              <p style={{ textAlign: "center", marginTop: 20 }}>Nenhuma instrução encontrada.</p>
-            )}
+                <div style={styles.dateCol}>
+                  <input
+                    type="text"
+                    value={
+                      item.expected_date
+                        ? new Date(item.expected_date).toLocaleDateString()
+                        : ""
+                    }
+                    readOnly
+                    style={styles.dateColInput}
+                  />
+                </div>
+                <div style={styles.dateCol}>
+                  <input
+                    type="text"
+                    value={
+                      item.finished_date
+                        ? new Date(item.finished_date).toLocaleDateString()
+                        : ""
+                    }
+                    readOnly
+                    style={styles.dateColInput}
+                  />
+                </div>
+                <div style={styles.statusCol}>
+                  <span style={styles.statusBadge}>{item.status}</span>
+                </div>
+              </div>
+            ))}
           </div>
         </>
       )}
 
       <div style={styles.buttonContainer}>
         <li style={styles.addInstructionButton}>
-          <Link to="/admin" style={{ color: "white", textDecoration: "none" }}>Voltar</Link>
+          <Link to="/admin" style={{ color: "white", textDecoration: "none" }}>
+            Voltar
+          </Link>
         </li>
         {user?.role === "ADMIN" && (
           <li style={styles.addInstructionButton}>
             <button
               onClick={() => navigate("/admin/createInstruction")}
-              style={{ color: "white", textDecoration: "none", border: "none", background: "transparent", cursor: "pointer" }}
+              style={{
+                color: "white",
+                textDecoration: "none",
+                border: "none",
+                background: "transparent",
+                cursor: "pointer",
+              }}
             >
               Adicionar Instrução
             </button>
