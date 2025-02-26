8bea5576f6e191fee87763d2a6fe33243d03e98d
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-02-25 21:09:26 -0300

    Fix: readings and completionWorks
    Remove readings and completionWorks of datacontext

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -3,14 +3,10 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 import { useAuth } from "./AuthContext";
 import { getInstructions } from "../services/InstructionsService";
 import { getVisitations } from "../services/VisitationsService";
-import { getReadings } from "../services/ReadingsService";
-import { getCompletionWorks } from "../services/CompletionWorksService";
 
 interface DataContextType {
   instructions: any[];
   visitations: any[];
-  readings: any[];
-  completionWorks: any[];
   loading: boolean;
   refreshData: () => void;
 }
@@ -21,25 +17,19 @@ export const DataProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const { user } = useAuth();
   const [instructions, setInstructions] = useState<any[]>([]);
   const [visitations, setVisitations] = useState<any[]>([]);
-  const [readings, setReadings] = useState<any[]>([]);
-  const [completionWorks, setCompletionWorks] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     if (!user) return;
     setLoading(true);
     try {
-      const [instructionsData, visitationsData, readingsData, completionWorksData] = await Promise.all([
+      const [instructionsData, visitationsData ] = await Promise.all([
         getInstructions(user.role, user.id),
         getVisitations(user.role, user.id),
-        getReadings(user.role, user.id),
-        getCompletionWorks(user.role, user.id),
       ]);
 
       setInstructions(instructionsData);
       setVisitations(visitationsData);
-      setReadings(readingsData);
-      setCompletionWorks(completionWorksData);
     } catch (error) {
       console.error("Erro ao buscar dados:", error);
     } finally {
@@ -52,7 +42,7 @@ export const DataProvider: React.FC<{ children: React.ReactNode }> = ({ children
   }, [user, fetchData]);
 
   return (
-    <DataContext.Provider value={{ instructions, visitations, readings, completionWorks, loading, refreshData: fetchData }}>
+    <DataContext.Provider value={{ instructions, visitations, loading, refreshData: fetchData }}>
       {children}
     </DataContext.Provider>
   );
