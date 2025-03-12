f07093cc87225d975a3224c49ebb8d7d1d5914d6
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-03-11 21:14:37 -0300

    Fix: UserRequirement

--- src/pages/user/requirements/UserRequirements.tsx
+++ src/pages/user/requirements/UserRequirements.tsx
@@ -3,36 +3,34 @@ import { useAuth } from "../../../context/AuthContext";
 import useDocumentTitle from "../../../hooks/PageTitle";
 import { useEffect, useState } from "react";
 import { styles } from "./styles";
-import { getRequirements } from "../../../services/LodgeService";
 import UploadModal from "../../../components/modalUpload/UploadModal";
-// import UploadModal from "../../../components/modalUpload/UploadModal"; // Modal de Upload
-
+import { getUserRequirements } from "../../../services/InstructionsService";
+import React from "react";
 interface LodgeRequirementsProps {
-  filter: "user" | "expected_date";
+  filter: "expected_date";
 }
 
-interface Requirement {
+interface UserRequirement {
   id: number;
+  user_id: number;
+  requirement_id: number;
   lodge_id: number;
   status: string;
+  finished_date: string | null;
+  approved_date: string | null;
+  voucher_id?: number | null;
+  expected_date: string;
+  name: string;
+  user_name: string;
+  requirements_type: string;
   is_voucher: boolean;
-  user: {
-    id: number;
-    name: string;
-  };
-  requirements: {
-    name: string;
-    expected_date: string;
-    approved_date: string;
-    requirements_type: string;
-  };
 }
 
 export default function UserRequirements({ filter }: LodgeRequirementsProps) {
   useDocumentTitle("Requisitos");
   const { user } = useAuth();
   const navigate = useNavigate();
-  const [requirements, setRequirements] = useState<Requirement[]>([]);
+  const [userRequirements, setUserRequirements] = useState<UserRequirement[]>([]);
   const [loading, setLoading] = useState(false);
   const [selectedRequirementId, setSelectedRequirementId] = useState<number | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -43,16 +41,12 @@ export default function UserRequirements({ filter }: LodgeRequirementsProps) {
     const fetchRequirements = async () => {
       setLoading(true);
       try {
-        if (isNaN(Number(user.lodge_id))) {
-          throw new Error("lodge_id inválido: " + user.lodge_id);
-        }
+        const data: any = await getUserRequirements(user.id);
+        const formattedData: any = Array.isArray(data) ? data : [data];
 
-        // Para USER, sempre filtra por "expected_date"
-        const appliedFilter = user.role === "USER" ? "expected_date" : filter;
-        const data: any = await getRequirements(user.lodge_id, appliedFilter);
-        const formattedData = Array.isArray(data) ? data : [data];
+        // Se o usuário for "USER", filtramos apenas os requisitos que pertencem a ele
 
-        setRequirements(formattedData);
+        setUserRequirements(formattedData);
       } catch (error) {
         console.error("Erro ao buscar requisitos:", error);
       } finally {
@@ -80,7 +74,7 @@ export default function UserRequirements({ filter }: LodgeRequirementsProps) {
     <div style={styles.container}>
       {loading ? (
         <p style={{ textAlign: "center", marginTop: 20 }}>Carregando requisitos...</p>
-      ) : requirements.length === 0 ? (
+      ) : userRequirements.length === 0 ? (
         <p style={{ textAlign: "center", marginTop: 20 }}>Nenhum requisito encontrado.</p>
       ) : (
         <div style={styles.requirementsTable}>
@@ -93,16 +87,16 @@ export default function UserRequirements({ filter }: LodgeRequirementsProps) {
             <div style={styles.requirementsCol}>Comprovante</div>
             <div style={styles.requirementsCol}>Grau</div>
           </div>
-          {requirements.map((item) => (
+          {userRequirements.map((item) => (
             <div key={`${item.id}-${item.lodge_id}`} style={styles.requirementsRow}>
-              <div style={styles.requirementsCol}>{item.requirements.name}</div>
-              <div style={styles.requirementsCol}>{item.user.name}</div>
+              <div style={styles.requirementsCol}>{item.name}</div>
+              <div style={styles.requirementsCol}>{item.user_name}</div>
               <div style={styles.dateCol}>
-                {new Date(item.requirements.expected_date).toLocaleDateString()}
+                {new Date(item.expected_date).toLocaleDateString()}
               </div>
               <div style={styles.dateCol}>
-                {item.requirements.approved_date
-                  ? new Date(item.requirements.approved_date).toLocaleDateString()
+                {item.finished_date
+                  ? new Date(item.finished_date).toLocaleDateString()
                   : ""}
               </div>
               <div style={{ ...styles.statusCol, color: getStatusColor(item.status) }}>
@@ -113,22 +107,22 @@ export default function UserRequirements({ filter }: LodgeRequirementsProps) {
                 <button
                   style={{
                     ...styles.uploadButton,
-                    opacity: item.status === "PENDENTE" ? 1 : 0.5,
-                    cursor: item.status === "PENDENTE" ? "pointer" : "not-allowed",
+                    opacity: item.status === "PENDENTE" && item.is_voucher ? 1 : 0.5,
+                    cursor: item.status === "PENDENTE" && item.is_voucher ? "pointer" : "not-allowed",
                     textAlign: "center" as const,
                   }}
-                  disabled={item.status !== "PENDENTE"} // Apenas requisitos pendentes podem receber comprovante
+                  disabled={!(item.status === "PENDENTE" && item.is_voucher)} 
                   onClick={() => {
-                    if (item.status === "PENDENTE") {
+                    if (item.status === "PENDENTE" && item.is_voucher) {
                       setSelectedRequirementId(item.id);
                       setIsModalOpen(true);
                     }
                   }}
                 >
-                  Enviar Comprovante
+                  Upload
                 </button>
               </div>
-              <div>{item.requirements.requirements_type}</div>
+              <div>{item.requirements_type}</div>
             </div>
           ))}
           {/* Modal para Upload do Comprovante */}
