0b420e9aaf006238af30c1fa0cab17a88cf7e798
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-03-11 21:16:24 -0300

    Fix: lines
    Remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
 import Login from "./pages/login/Login";
 //import Admin from "./pages/admin/Admin";
-import InstructionView from "./views/instructions/InstructionsView";
-import CreateInstruction from "./pages/tasks/TaskPage";
 import Register from "./pages/register/Register";
-import UpdateInstruction from "./pages/tasks/requirements/updateInstruction/UpdateInstruction";
 import AdminView from "./views/admin/AdminView";
 import UserView from "./views/user/UserView";
+import React from "react";
 
 const PrivateRoute = ({ children, role }: { children: JSX.Element; role: 'ADMIN' | 'USER' }) => {
   const { user }: any = useAuth();
@@ -39,10 +37,7 @@ function App() {
       />
       <Route path="/admin" element={<PrivateRoute role="ADMIN"><AdminView /></PrivateRoute>} />
       {/* Rotas Instructions */}
-      <Route path="/admin/createInstruction" element={<PrivateRoute role="ADMIN"><CreateInstruction /></PrivateRoute>} />
-      <Route path="/admin/updateInstruction/:id" element={<PrivateRoute role="ADMIN"><UpdateInstruction /></PrivateRoute>} />
       <Route path="/home" element={<PrivateRoute role="USER"><UserView /></PrivateRoute>} />
-      <Route path="/instructions" element={<InstructionView />} />
       <Route
         path="*"
         element={<Navigate to={user ? (user.role === 'ADMIN' ? '/admin' : '/home') : '/login'} replace />}
