0ae76bb62b2930dd7db320ada06fd3021a8acf39
Marcos9868 <marcos_m_ferreira@outlook.com>
2025-03-11 21:11:52 -0300

    Fix: URL_BASE
    Remove commentary

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL_BASE = 'http://localhost:4000/v1'; // Substitua pela URL da sua API
+const API_URL_BASE = 'http://localhost:4000/v1'; 
 
 const api = axios.create({
   baseURL: API_URL_BASE,
